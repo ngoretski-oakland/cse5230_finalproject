a8b99d6d907bf591aea13ff14102ad3aba7ed224
Nathan Goretski <nwgorets@mtu.edu>
2017-12-05 18:33:04 -0500

    adding music player

--- app/src/main/java/edu/csi5230/ngoretski/finalproject/MusicActivity.java
+++ app/src/main/java/edu/csi5230/ngoretski/finalproject/MusicActivity.java
@@ -1,13 +1,42 @@
 package edu.csi5230.ngoretski.finalproject;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MusicActivity extends AppCompatActivity {
+
+    Button jingleBells;
+
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+
+        jingleBells = (Button) findViewById(R.id.button_music_jingle);
+
+        jingleBells.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (player != null) {
+                    player.stop();
+                }
+
+                player = MediaPlayer.create(MusicActivity.this, R.raw.jinglebells);
+                player.start();
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (player != null) {
+            player.stop();
+        }
     }
 }
